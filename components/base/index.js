97ed9fd7d78a1b1ae931b885f889838dac96f538
Arya Irama Wahono <aryairama987@gmail.com>
2021-10-22 22:36:47 +0700

    add meta

--- components/base/index.js
+++ components/base/index.js
@@ -5,3 +5,4 @@ export { default as CardSkill } from './CardSkill';
 export { default as TimelineContainer } from './Timeline/TimelineContainer';
 export { default as LeftTimeline } from './Timeline/LeftTimeline';
 export { default as RightTimeline } from './Timeline/RightTimeline';
+export { default as Meta } from './Meta';
--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,15 @@ import '../styles/globals.css';
 import 'aos/dist/aos.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'devicon/devicon.min.css';
+import { Meta } from '../components/base';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Meta />
+      <Component {...pageProps} />
+    </>
+  );
 }
 MyApp.getInitialProps = wrapper.getInitialAppProps((store) => async ({ Component, ctx }) => {
   return {
