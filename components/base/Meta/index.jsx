c339bbf0b97ff32bab6b0c0e6d54e77fb51c7c7b
Arya Irama Wahono <aryairama987@gmail.com>
2021-10-23 06:59:19 +0700

    fix overflow scroll x for aos animation

--- components/base/Meta/index.jsx
+++ components/base/Meta/index.jsx
@@ -1,6 +1,10 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 const Meta = () => {
+  useEffect(() => {
+    document.body.classList.add('overflow-x-hidden');
+  });
   return (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1,0, user-scalable=no" />
