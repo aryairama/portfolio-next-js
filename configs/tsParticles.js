71b45166ca0043c766974bda7104ed3a880739bd
Arya Irama Wahono <aryairama987@gmail.com>
2021-10-24 10:04:04 +0700

    update configuration particles

--- configs/tsParticles.js
+++ configs/tsParticles.js
@@ -1,5 +1,5 @@
 export const headerParticles = {
-  fps_limit: 60,
+  fpsLimit: 60,
   interactivity: {
     detectsOn: 'canvas',
     events: {
@@ -7,7 +7,16 @@ export const headerParticles = {
       onHover: { enable: true, mode: 'repulse' },
       resize: true,
     },
-    modes: { push: { particles_nb: 4 }, repulse: { distance: 100, duration: 0.4 } },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: { particles_nb: 4 },
+      repulse: { distance: 100, duration: 0.4 },
+    },
   },
   particles: {
     color: { value: '#ffffff' },
@@ -21,7 +30,7 @@ export const headerParticles = {
       speed: 2,
       straight: false,
     },
-    number: { density: { enable: true, area: 800 }, value: 80 },
+    number: { density: { enable: true, value_area: 800 }, value: 80 },
     opacity: { value: 0.5 },
     shape: { type: 'circle' },
     size: { random: true, value: 5 },
