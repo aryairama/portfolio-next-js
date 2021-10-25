9710b0b42159fa5da899fc86043dc5d95e4d24d3
Arya Irama Wahono <aryairama987@gmail.com>
2021-10-25 16:21:39 +0700

    change props params to options in particles

--- components/module/Header/index.jsx
+++ components/module/Header/index.jsx
@@ -7,12 +7,9 @@ import { headerParticles } from '../../../configs/tsParticles';
 import { TextIconContact, ListIconContactContainer, ListIconContact } from '../../base';
 
 const Header = ({ name, job, avatar, phoneNumber, email, address, contacts }) => {
-  if (process.browser) {
-    require('pathseg');
-  }
   return (
     <div className={style['header-container']}>
-      <ParticlesCsr params={headerParticles} id="headerParticles" className={style['header-particles']} />
+      <ParticlesCsr options={headerParticles} id="headerParticles" className={style['header-particles']} />
       <div className={style['header-content']}>
         <div className={style['header-content-left']}>
           <div
