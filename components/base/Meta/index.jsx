2820c5cb78cd1e690685a3f06bfb334888e7fdc1
Arya Irama Wahono <aryairama987@gmail.com>
2021-10-26 22:05:34 +0700

    alert jika dibuka di ie 11

--- components/base/Meta/index.jsx
+++ components/base/Meta/index.jsx
@@ -1,19 +1,43 @@
 import Head from 'next/head';
+import { detect } from 'detect-browser';
+import style from './Meta.module.css';
+import { useEffect, useState } from 'react';
 
 const Meta = () => {
+  const [support, setSupport] = useState(true);
+  useEffect(() => {
+    const browser = detect();
+    if (browser.name === 'ie') {
+      setSupport(false);
+    }
+    setTimeout(() => {
+      setSupport(true);
+    }, 5000);
+  }, []);
   return (
-    <Head>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
-      />
-      <meta
-        name="description"
-        content="Arya Irama Wahono is a full stack web developer who always likes to experiment and never gives up."
-      />
-      <meta name="robots" content="index, follow" />
-      <title>Arya Irama Wahono</title>
-    </Head>
+    <>
+      {support === false && (
+        <div data-aos="fade-left" className={style['alert-not-support']}>
+          <div>
+            Please open it with Chrome, Firefox, Opera and Microsoft Edge browsers. Our website does not fully support
+            internet explorer browser
+          </div>
+          <i className="fas fa-times" onClick={() => setSupport(true)}></i>
+        </div>
+      )}
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="Arya Irama Wahono is a full stack web developer who always likes to experiment and never gives up."
+        />
+        <meta name="robots" content="index, follow" />
+        <title>Arya Irama Wahono</title>
+      </Head>
+    </>
   );
 };
 
